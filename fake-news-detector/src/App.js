37d23e40f51bc00d6c1a370eafca7f39d323308a
Aminat Ahmed <oahmedaminat@gmail.com>
2022-12-19 00:02:56 +0100

    Hosting and final fixes

--- fake-news-detector/src/App.js
+++ fake-news-detector/src/App.js
@@ -6,16 +6,19 @@ function App() {
     "https://punchng.com/pdp-fate-uncertain-as-party-g5-govs-face-off-lingers/",
     "https://punchng.com/fayose-olujimis-names-missing-as-pdp-names-ekiti-pcc/",
     "https://www.vanguardngr.com/2022/11/asuu-fg-face-off-not-yet-over-lecturers/",
+    "https://www.nytimes.com/live/2022/12/18/sports/argentina-france-world-cup-final-score",
+    "https://guardian.ng/sport/argentina-beat-france-on-penalties-to-win-world-cup/",
   ];
 
   const [news, setNews] = useState("");
 
-  const newsCheck = () => {
+  const newsCheck = (e) => {
+    e.preventDefault();
     return links.map((item) => {
       if (item.toLowerCase().includes(news.toLowerCase())) {
         alert("This news is confirmed to be TRUE!!!");
       } else {
-        alert("This news is confirmed to be TRUE!!!")
+        alert("This news is confirmed to be FAKE!!!")
       }
     });
   };
---
+++ fake-news-detector/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCkn_aLGj9EBo966c05P7-tX3_KHFrv3-Q",
+  authDomain: "news-detector.firebaseapp.com",
+  projectId: "news-detector",
+  storageBucket: "news-detector.appspot.com",
+  messagingSenderId: "107169219822",
+  appId: "1:107169219822:web:f462f4251520da2b889fff"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
